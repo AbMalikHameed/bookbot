0f71f3d4c1fe76f2dfc86e8ced2b282801433cd7
AbMalikHameed <abmalik.hameed99@gmail.com>
2026-01-04 16:36:41 +0000

    added function in stats.py to get a character count. Imported function in main.py. main.py now prints word count and count for each character.

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
+#IMPORT FUNCTIONS FROM STATS.PY
 from stats import word_count
+from stats import get_character_count
 
+#PRINTS PUT WORD COUNT AND THE COUNT OF ALL CHARACTERS OF A BOOK
 def main():
     book = get_book_text("books/frankenstein.txt")
+
+    dictionary = get_character_count(book)
     
     print(word_count(book))
+    print(dictionary)
 
+
+#GETS ALL TEXT FROM A BOOK
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
         file_contents = f.read()
--- stats.py
+++ stats.py
@@ -1,3 +1,5 @@
+#FUNCTIONS TO COUNT HOW MANY WORDS IN A BOOK
+#GIVEN A BOOK AS AN INPUT
 def word_count(book):
     words = book.split()
     count = 0
@@ -6,3 +8,22 @@ def word_count(book):
         count +=1
     
     return f"Found {count} total words"
+
+#FUNCTION TO CREATE A DICTIONARY OF HOW MANY TIME
+#EACH CHARACTER APPEARS IN THE BOOK GIVEN A BOOK AS INPUT
+def get_character_count(book):
+    unique_characters = {}
+
+#ITTERATES OVER ALL CHARCTERS IN THE BOOK
+    for char in book:
+        #MAKES CHARCTERS LOWER CASE
+        char = char.lower()
+
+        #BUILD DICTIONARY OF CHARACTERS AND COUNTS
+        if char in unique_characters:
+            unique_characters[char] += 1
+        else:
+            unique_characters[char] = 1
+    
+
+    return unique_characters
